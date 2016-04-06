9807e4f711994729da5a91fe6f053e01f88860e7
chedahuang <chedahuang.icloud.com>
2016-04-06 09:46:05 +0800

    add HcdLotteryView

---
+++ hcdLotteryView/hcdLotteryView/HcdLotteryView.h
@@ -0,0 +1,13 @@
+//
+//  HcdLotteryView.h
+//  hcdLotteryView
+//
+//  Created by polesapp-hcd on 16/4/6.
+//  Copyright © 2016年 Polesapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HcdLotteryView : UIView
+
+@end
---
+++ hcdLotteryView/hcdLotteryView/HcdLotteryView.m
@@ -0,0 +1,21 @@
+//
+//  HcdLotteryView.m
+//  hcdLotteryView
+//
+//  Created by polesapp-hcd on 16/4/6.
+//  Copyright © 2016年 Polesapp. All rights reserved.
+//
+
+#import "HcdLotteryView.h"
+
+@implementation HcdLotteryView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
